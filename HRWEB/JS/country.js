4277462177eaf962c81d6affc46aea9de3589574
Nosheenkalwar <bscs2312166@szabist.pk>
2025-05-09 05:29:38 +0000

    Create Script and View

---
+++ HRWEB/JS/country.js
@@ -0,0 +1,25 @@
+
+
+const country_link = "https://supreme-waffle-r4wq66j4xrvr3x99p-6006.app.github.dev/country";
+
+fetch(country_link).then(response=>{
+    if(!response.ok)
+        throw new Error("Failed to Fetch data from given URL");
+    return response.json();
+}).then(data=>{
+    const tbody = document.querySelector("#countrytable tbody");
+
+    data.forEach(c=>{
+        const row = document.createElement("tr");
+
+        row.innerHTML=`
+         <td>${c.country_id}</td>
+         <td>${c.country_name}</td>
+         <td>${c.region_id}</td>
+        `;
+
+        tbody.appendChild(row);
+    });
+}).catch(err=>{
+    console.log(err.message);
+});
